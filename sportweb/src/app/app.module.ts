e8d2b29d409de767105f4bb13b35bd2581e49d3d
sebastian quintero <sebasqc2@gmail.com>
2020-07-05 05:55:13 -0500

    arquitectura mvc con ejemplo

--- sportweb/src/app/app.module.ts
+++ sportweb/src/app/app.module.ts
@@ -1,18 +1,25 @@
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserService } from './shared/services/user.service';
+import { UsersComponent } from './views/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
